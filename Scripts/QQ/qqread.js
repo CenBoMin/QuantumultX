2028cc778ef0121dd631c5417e35f64cfff8e287
CenBomin <70682745+chenborming@users.noreply.github.com>
2020-12-16 05:10:46 +0800

    data

--- Scripts/QQ/qqread.js
+++ Scripts/QQ/qqread.js
@@ -1,22 +1,4 @@
-/* ziye
-
-本人github地址     https://github.com/ziye12/JavaScript
-转载请备注个名字，谢谢
-
-11.25 增加 阅读时长上传，阅读金币，阅读随机金币
-11.25 修复翻倍宝箱不同时领取的问题.增加阅读金币判定
-11.25 修复阅读时长问题，阅读金币问题，请重新获取时长cookie
-11.26 随机金币只有一次，故去除，调整修复阅读金币问题，增加时长上传限制
-11.26 增加领取周时长奖励
-11.26 增加结束命令
-11.27 调整通知为，成功开启宝箱再通知
-11.28 修复错误
-11.29 更新 支持action.默认每天21点到21点20通知
-12.2 修复打卡问题
-12.3 缩短运行时间，由于企鹅读书版本更新.请手动进去看一次书
-12.3 调整推送时间为12点和24点左右
-12.6 精简打印通知
-12.7 解决1金币问题，务必重新获取一次更新body
+/*
 
 ⚠️cookie获取方法：
 
@@ -24,15 +6,11 @@
 
 进书库选择一本书,看10秒以下,然后退出，获取时长url和时长header以及更新body，看书一定不能超过10秒
 
-
-
 Secrets对应关系如下，多账号默认换行
 
 qqreadbodyVal         👉   QQREAD_BODY
 qqreadtimeurlVal      👉   QQREAD_TIMEURL
 qqreadtimeheaderVal   👉   QQREAD_TIMEHD
-
-
 
 ⚠️宝箱奖励为20分钟一次，自己根据情况设置定时，建议设置11分钟一次
 
@@ -45,23 +23,6 @@ https:\/\/mqqapi\.reader\.qq\.com\/log\/v4\/mqq\/track url script-request-body h
 
 #企鹅读书获取时长cookie
 https:\/\/mqqapi\.reader\.qq\.com\/mqq\/addReadTimeWithBid? url script-request-header https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/qqreads.js
-
-
-############## loon
-
-//企鹅读书获取更新body
-http-request https:\/\/mqqapi\.reader\.qq\.com\/log\/v4\/mqq\/track script-path=https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/qqreads.js,requires-body=true, tag=企鹅读书获取更新body
-
-//企鹅读书获取时长cookie
-http-request https:\/\/mqqapi\.reader\.qq\.com\/mqq\/addReadTimeWithBid? script-path=https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/qqreads.js, requires-header=true, tag=企鹅读书获取时长cookie
-
-############## surge
-
-//企鹅读书获取更新body
-企鹅读书获取更新body = type=http-request,pattern=https:\/\/mqqapi\.reader\.qq\.com\/log\/v4\/mqq\/track,script-path=https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/qqreads.js,
-
-//企鹅读书获取时长cookie
-企鹅读书获取时长cookie = type=http-request,pattern=https:\/\/mqqapi\.reader\.qq\.com\/mqq\/addReadTimeWithBid?,script-path=https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/qqreads.js,
 
 
 */
@@ -86,15 +47,21 @@ const TIME = 30; // 单次时长上传限制，默认5分钟
 const maxtime = 12; // 每日上传时长限制，默认12小时
 const wktimess = 1200; // 周奖励领取标准，默认1200分钟
 
-const qqreadbdArr = [];
+const qqreadbdArr = []; //qq阅读body
 let qqreadbodyVal = "";
-const qqreadtimeurlArr = [];
+const qqreadtimeurlArr = [];//qq阅读时长
 let qqreadtimeurlVal = "";
-const qqreadtimehdArr = [];
+const qqreadtimehdArr = [];//qq阅读时长头
 let qqreadtimeheaderVal = "";
+
 let qqreadBD = [];
 let qqreadtimeURL = [];
 let qqreadtimeHD = [];
+
+
+/////////////////////////cookie获取模块////////////////////////////
+
+
 if ($.isNode()) {
   if (process.env.COOKIES_SPLIT) {
     COOKIES_SPLIT = process.env.COOKIES_SPLIT;
@@ -104,6 +71,8 @@ if ($.isNode()) {
       COOKIES_SPLIT
     )} =============\n`
   );
+
+
   if (
     process.env.QQREAD_BODY &&
     process.env.QQREAD_BODY.indexOf(COOKIES_SPLIT) > -1
@@ -131,6 +100,9 @@ if ($.isNode()) {
     qqreadtimeHD = process.env.QQREAD_TIMEHD.split();
   }
 }
+
+
+
 
 if ($.isNode()) {
   Object.keys(qqreadBD).forEach((item) => {
@@ -163,28 +135,55 @@ if ($.isNode()) {
   qqreadtimehdArr.push($.getdata("qqreadtimehd"));
 }
 
+
+
+
+////////////////////////////////   cookie start   //////////////////////////////////////////////
+
+
 if ((isGetCookie = typeof $request !== "undefined")) {
   GetCookie();
 $.done();
 }
 
+//GetCookie 函数
+
 function GetCookie() {
 
 if ($request && $request.url.indexOf("addReadTimeWithBid?") >= 0) {
+
+
+//qqreadtimeurlVal
+
     const qqreadtimeurlVal = $request.url;
+
     if (qqreadtimeurlVal) $.setdata(qqreadtimeurlVal, "qqreadtimeurl");
+
     $.log(
       `[${jsname}] 获取时长url: 成功,qqreadtimeurlVal: ${qqreadtimeurlVal}`
     );
- $.msg(jsname, `获取时长url: 成功🎉`, ``);
+
+    $.msg(jsname, `获取时长url: 成功🎉`, ``);
+
+
+
+//qqreadtimeheaderVal
+
     const qqreadtimeheaderVal = JSON.stringify($request.headers);
+
     if (qqreadtimeheaderVal) $.setdata(qqreadtimeheaderVal, "qqreadtimehd");
+
     $.log(
       `[${jsname}] 获取时长header: 成功,qqreadtimeheaderVal: ${qqreadtimeheaderVal}`
     );
+
     $.msg(jsname, `获取时长header: 成功🎉`, ``);
   }
   else if ($request &&$request.body.indexOf("bookDetail_bottomBar_read_C")>=0&&$request.body.indexOf("topBar_left_back_C")<0&&$request.body.indexOf("bookRead_dropOut_shelfYes_C")<0){
+
+
+//qqreadbodyVal
+
     const qqreadbodyVal = $request.body;
     if (qqreadbodyVal) $.setdata(qqreadbodyVal, "qqreadbd");
     $.log(
@@ -196,7 +195,11 @@ if ($request && $request.url.indexOf("addReadTimeWithBid?") >= 0) {
 
 }
 
+////////////////////////////////   cookie end   //////////////////////////////////////////////
 
+
+
+////////////////////////////////   All function    //////////////////////////////////
 
 let K = 0;
 all();
@@ -271,6 +274,14 @@ function all() {
   }
 }
 
+
+
+////////////////////////////////   All function end   //////////////////////////////////
+
+
+/////////////////////   function 1   //////////////////////
+
+
 // 任务列表
 function qqreadtask() {
   return new Promise((resolve, reject) => {
@@ -305,6 +316,11 @@ function qqreadtask() {
   });
 }
 
+/////////////////////   function 2   //////////////////////
+
+
+
+
 
 // 更新
 function qqreadtrack() {
@@ -326,7 +342,7 @@ function qqreadtrack() {
 }
 
 
-
+/////////////////////   function 3  //////////////////////
 
 // 用户名
 function qqreadinfo() {
@@ -349,6 +365,10 @@ function qqreadinfo() {
   });
 }
 
+
+
+/////////////////////   function 4   //////////////////////
+
 // 阅豆签到
 function qqreadtake() {
   return new Promise((resolve, reject) => {
@@ -369,6 +389,9 @@ function qqreadtake() {
     });
   });
 }
+
+
+/////////////////////   function 5   //////////////////////
 
 // 阅读时长任务
 function qqreadconfig() {
@@ -407,6 +430,11 @@ function qqreadtime() {
     });
   });
 }
+
+
+/////////////////////   function 6   //////////////////////
+
+
 
 // 阅读金币1
 function qqreadssr1() {
@@ -471,6 +499,10 @@ function qqreadssr3() {
   });
 }
 
+
+
+/////////////////////   function 7   //////////////////////
+
 // 金币签到
 function qqreadsign() {
   return new Promise((resolve, reject) => {
@@ -511,6 +543,13 @@ function qqreadsign2() {
   });
 }
 
+
+
+/////////////////////   function 8   //////////////////////
+
+
+
+
 // 每日阅读
 function qqreaddayread() {
   return new Promise((resolve, reject) => {
@@ -532,6 +571,13 @@ function qqreaddayread() {
   });
 }
 
+
+
+
+/////////////////////   function 9   //////////////////////
+
+
+
 // 视频奖励
 function qqreadvideo() {
   return new Promise((resolve, reject) => {
@@ -551,6 +597,13 @@ function qqreadvideo() {
     });
   });
 }
+
+
+
+/////////////////////   function 10   //////////////////////
+
+
+
 
 // 宝箱奖励
 function qqreadbox() {
@@ -612,6 +665,14 @@ function qqreadwktime() {
   });
 }
 
+
+
+
+/////////////////////   function 10   //////////////////////
+
+
+
+
 // 本周阅读时长奖励任务
 function qqreadpick() {
   return new Promise((resolve, reject) => {
@@ -657,6 +718,13 @@ function qqreadpick() {
   });
 }
 
+
+
+/////////////////////   msg function    //////////////////////
+
+
+
+
 function showmsg() {
   tz += `\n\n========= 脚本执行-北京时间(UTC+8)：${new Date(
     new Date().getTime() + 8 * 60 * 60 * 1000
@@ -692,5 +760,5 @@ function showmsg() {
 }
 
 
-// prettier-ignore
+/////////////////////  prettier-ignore   /////////////////////
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
